b23e3d0912156baa0898ea259e0fa8d1b8d71794
Saulo Veiga <sauloveigr@hotmail.com>
2021-04-28 22:01:53 -0300

    Finished the carousel

--- js/main-carousel.js
+++ js/main-carousel.js
@@ -1,14 +1,12 @@
 const buttonRight = document.querySelector('.button-arrow.-right'); 
 const buttonLeft = document.querySelector('.button-arrow.-left'); 
 const elements = document.querySelector('.elements');
-let pixels = 0  // "constante" que o valor pode ser alterado
-
+let pixels = 0 
 
 buttonRight.addEventListener('click', function() { 
     pixels = pixels + 100
     elements.style = `transform: translateX(${pixels}px);`;
 })
-
 
 buttonLeft.addEventListener('click', function() {
     pixels = pixels - 100
